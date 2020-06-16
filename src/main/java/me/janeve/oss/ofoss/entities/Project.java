fa3601afe09df9c302e1512517f9160f3d0c6c88
janeve.george <janeve.george@gmail.com>
2020-06-16 23:03:29 +0530

    Added links to opoen project details view

--- src/main/java/me/janeve/oss/ofoss/entities/Project.java
+++ src/main/java/me/janeve/oss/ofoss/entities/Project.java
@@ -16,6 +16,14 @@ public class Project {
         this.owner = owner;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
