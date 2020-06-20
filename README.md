63cf556890f61518859498ec468afa022bae28d8
janeve.george <janeve.george@gmail.com>
2020-06-20 07:25:41 +0530

    Adding GNU GPL License texts

--- README.md
+++ README.md
@@ -7,3 +7,24 @@ Currently I am working as Manager - Product Development in Bangalore, India.
 
 ## Website
 http://www.janeve.me
+
+# License
+All code found in this repository is licensed under GNU GPL v3 <https://www.gnu.org/licenses/>.
+
+    Open3PPManager v0.1-SNAPSHOT (https://github.com/janevegeorge/Open3PPManager)
+    Copyright 2020 Janeve.Me (http://www.janeve.me)
+    
+    This file is part of Open3PPManager
+    
+    Open3PPManager is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Open3PPManager is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Open3PPManager.  If not, see <https://www.gnu.org/licenses/>.
--- src/main/java/me/janeve/oss/o3ppm/entities/SoftwareData.java
+++ src/main/java/me/janeve/oss/o3ppm/entities/SoftwareData.java
@@ -1,10 +1,28 @@
+/*!
+ * Open3PPManager v0.1-SNAPSHOT (https://github.com/janevegeorge/Open3PPManager)
+ * Copyright 2020 Janeve.Me (http://www.janeve.me)
+ *
+ * This file is part of "Open3PPManager".
+ *
+ * Open3PPManager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Open3PPManager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Open3PPManager. If not, see <http://www.gnu.org/licenses/>.
+ */
 package me.janeve.oss.o3ppm.entities;
 
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
